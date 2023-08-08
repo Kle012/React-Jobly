8c8f2380758f69e181297b2b6b57163f2f1e119b
Kle012 <kimhle.0102@gmail.com>
2023-08-07 18:49:20 -0700

    Simple welcome page

---
+++ src/homepage/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/** Simple welcome message 
+ * 
+*/
+
+const Home = () => {
+    return (
+        <div className="homepage">
+            <h1>Welcome!</h1>
+        </div>
+    )
+}
+
+export default Home;
