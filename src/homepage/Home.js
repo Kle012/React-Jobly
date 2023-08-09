e843e201b82f1fbff8c9148a18b470b828cc2bdb
Kle012 <kimhle.0102@gmail.com>
2023-08-08 18:54:50 -0700

    useContext(UserContext) -> show different messages if the users is logged in or out

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../auth/UserContext";
+import { Link } from "react-router-dom";
 
-/** Simple welcome message 
+/** Homepage of site
+ * 
+ * Shows welcome message or login/signup 
+ * 
+ * Route for /
+ * 
+ * Routes -> Homapage
  * 
 */
 
 const Home = () => {
+    const { currUser } = useContext(UserContext);
+
     return (
         <div className="homepage">
-            <h1>Welcome!</h1>
+            <div className="container">
+                <h1 className="title">Jobly</h1>
+                <p className="body">
+                    All the jobs in one, convenient place.
+                </p>
+                {currUser
+                    ? <h2>
+                        Welcome back, {currUser.firstName || currUser.username}
+                    </h2>
+                    : (
+                        <p>
+                            <Link className="" to='/login'>
+                                Log In
+                            </Link>
+                            <Link className="" to='/signup'>
+                                Sign Up
+                            </Link>
+                        </p>
+                    )}
+            </div>
         </div>
     )
 }
