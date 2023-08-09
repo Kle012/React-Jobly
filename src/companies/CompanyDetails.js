f38bc6460bd790cef8fe35b54a4bc6b0e671f9ab
Kle012 <kimhle.0102@gmail.com>
2023-08-08 18:53:25 -0700

    render JobListCard

--- src/companies/CompanyDetails.js
+++ src/companies/CompanyDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router-dom";
 import JoblyApi from "../api";
 import Loading from "../helpers/Loading";
 import JobCardList from "../jobs/JobCardList";
