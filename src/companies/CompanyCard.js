46d576d837b3a415341647df5df8a1fddb10a69d
Kle012 <kimhle.0102@gmail.com>
2023-08-08 18:53:08 -0700

    Company Card component

--- src/companies/CompanyCard.js
+++ src/companies/CompanyCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom";
 
 /** Show simple info about a company on the list
  * 
