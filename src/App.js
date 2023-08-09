ab9d4fa80ba4207bd9d7cec79a72609014c84eb2
Kle012 <kimhle.0102@gmail.com>
2023-08-08 18:50:27 -0700

    getCurrUser, LogIn, SignUp, LogOut, hasApplied, applyToJob, UserContext.Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,98 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navigation from './routes-navbar/Navigation';
 import Routes from './routes-navbar/Routes';
+import useLocalStorage from './hooks/useLocalStorage';
+import { jwt } from 'jsonwebtoken';
+import JoblyApi from './api';
+import { BrowserRouter } from 'react-router-dom';
+import UserContext from './auth/UserContext';
+import Loading from './helpers/Loading';
+
+/** Key name for token stored in localStorage for re-login */
+export const STORED_TOKEN = 'secret-token';
 
 function App() {
+  const [data, setData] = useState(false);
+  const [applicationId, setApplicationId] = useState(new Set([]));
+  const [currUser, setCurrUser] = useState(null);
+  const [token, setToken] = useLocalStorage(STORED_TOKEN);
+
+  useEffect(() => {
+    async function getCurrUser() {
+      if (token) {
+        try {
+          let { username } = jwt.decode(token);
+          JoblyApi.token = token;
+
+          let currUser = await JoblyApi.getCurrUser(username);
+          setCurrUser(currUser);
+          setApplicationId(new Set(currUser.applications));
+        }
+        catch (e) {
+          setCurrUser(null);
+        }
+      }
+      setData(true);
+    }
+
+    setData(false);
+    getCurrUser();
+  }, [token])
+
+  /** Log out */
+  const logout = () => {
+    setCurrUser(null);
+    setToken(null);
+
+  }
+
+  /** Sign up */
+  const signup = async (info) => {
+    try {
+      let token = await JoblyApi.signup(info);
+      setToken(token);
+      return { success: true }
+    } catch (error) {
+      return { success: false, error }
+    }
+  }
+
+  /** Log in */
+  const login = async (info) => {
+    try {
+      let token = await JoblyApi.login(info);
+      setToken(token);
+      return { success: true }
+    } catch (error) {
+      return { success: false, error }
+    }
+  }
+
+  /** Check if job has been applied */
+
+  const hasApplied= (id) => {
+    return applicationId.has(id);
+  }
+
+  /** Apply to job */
+  const applyToJob = (id) => {
+    if (hasApplied(id)) return;
+    JoblyApi.applyToJob(currUser.username, id);
+    setApplicationId(new Set([...applicationId, id]));
+  }
+
+  if (!data) return <Loading />;
+
   return (
-    <div className="App">
-      <Navigation />
-      <Routes />
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{ currUser, setCurrUser, hasApplied, applyToJob }}>
+        <div className='App'>
+          <Navigation logout={logout} />
+          <Routes login={login} signup={signup} />
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
